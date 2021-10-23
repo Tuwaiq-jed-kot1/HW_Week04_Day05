fe89c71149e7854673d366f0c82bb314a5e38ea0
RanaJaberAlrajhi <Rana.Alrajhi.jed.k1@tuwaiq.edu.sa>
2021-10-23 01:05:14 -0600

    DONE:)

---
+++ app/src/main/java/com/sumaya/myapplication/InfoFragment.kt
@@ -0,0 +1,28 @@
+package com.sumaya.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+
+class InfoFragment : Fragment() {
+    private lateinit var showInfo: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View?
+    {
+        val view = inflater.inflate(R.layout.fragment_info, container, false)
+        showInfo = view.findViewById(R.id.info)
+
+        parentFragmentManager.setFragmentResultListener("infoKey",this){key, info->
+            showInfo.text=  info.getString("data")
+        }
+
+        return view
+    }
+
+}
