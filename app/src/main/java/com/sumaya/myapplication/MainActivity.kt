54a9c51f097cb81a895fbc260630deddacbe4eec
SuzanneSaleem1 <Suzan.Alluhaybi.jed.k1@tuwaiq.edu.sa>
2021-10-23 18:19:46 +0300

    Week 4, day 5

--- app/src/main/java/com/sumaya/myapplication/MainActivity.kt
+++ app/src/main/java/com/sumaya/myapplication/MainActivity.kt
@@ -7,24 +7,24 @@ import androidx.fragment.app.FragmentTransaction
 import com.sumaya.myapplication.frag.FragOne
 import com.sumaya.myapplication.frag.FragTwo
 
-class MainActivity : AppCompatActivity() {
+
     class MainActivity : AppCompatActivity() {
         public override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
             val firstFragment = FragOne()
-            supportFragmentManager.beginTransaction().replace(R.id.frame, firstFragment)
+            supportFragmentManager.beginTransaction().add(R.id.frame, firstFragment)
                 .commit()
 
             val bundle = Bundle()
             val transaction = this.supportFragmentManager.beginTransaction()
-            val fragmentTwo = FragTwo()
-            fragmentTwo.arguments = bundle
-            transaction.replace(R.id.frame, fragmentTwo)
+            val fragmentOne = FragOne()
+            fragmentOne.arguments = bundle
+            transaction.replace(R.id.frame, fragmentOne)
             transaction.addToBackStack(null)
             transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
             transaction.commit()
         }
-    }}
+    }
 
 
