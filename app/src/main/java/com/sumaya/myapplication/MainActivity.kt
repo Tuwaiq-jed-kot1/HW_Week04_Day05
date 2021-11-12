a128a4c9f11c0bbfb69b4030b93e0ba54a53fe5e
badr <badr.fadul.jed.k1@tuwaiq.edu.sa>
2021-11-12 19:25:51 +0300

    hw

--- app/src/main/java/com/sumaya/myapplication/MainActivity.kt
+++ app/src/main/java/com/sumaya/myapplication/MainActivity.kt
@@ -1,119 +1,17 @@
 package com.sumaya.myapplication
 
-import android.app.AlertDialog
-import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.*
-import com.hbb20.CountryCodePicker
-import java.util.*
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
-
-    var Gender = arrayOf("Male", "Female")
-    private lateinit var spinner: Spinner
-    private lateinit var textView_massage:TextView
-
-    //1. Date
-    private lateinit var pickDate : TextView
-
-    //2. Country Code
-    private lateinit var ccp: CountryCodePicker
-    private var countryCode:String? = null
-    private var countryName:String? = null
-    private lateinit var showInfo : TextView
-    private lateinit var phone: EditText
-
-    private lateinit var send: Button
-    private lateinit var clear : Button
-    private lateinit var date :String
-
-
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        textView_massage = findViewById(R.id.massage)
-
-
-        spinner = findViewById(R.id.spinner_sample)
-        spinner.onItemSelectedListener = this
-
-
-        // Create an ArrayAdapter using a simple spinner layout and languages array
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, Gender)
-        // Set layout to use when the list of choices appear
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        // Set Adapter to Spinner
-        spinner.adapter = arrayAdapter
-
-
-        // 1. Date Dialog
-        pickDate= findViewById(R.id.pickDate)
-
-        //create object of Calendar
-        val calendar = Calendar.getInstance()
-        // add day of month
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-        val month = calendar.get(Calendar.MONTH)
-        val year = calendar.get(Calendar.YEAR)
-        pickDate.setOnClickListener {
-            DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ _, y,m,d ->
-                date = "$d/$m/$y"
-                pickDate.setText(date)
-            },year,month, day)
-                .show()
+        val mainfragment = MainFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.FragemtFrame, mainfragment)
+            commit()
         }
-
-        //2. Country Code
-        showInfo = findViewById(R.id.info)
-        phone = findViewById(R.id.phone)
-        ccp = findViewById(R.id.pickCode)
-        ccp.setOnCountryChangeListener {
-            countryCode = ccp.selectedCountryCode
-            countryName = ccp.selectedCountryName
-        }
-
-        //show Info
-        send = findViewById(R.id.send)
-        send.setOnClickListener {
-            var info = "Birthday: $date\n"
-            info += "Phone: +${countryCode.toString()+ phone.text}\n"
-            showInfo.setText(info)
-        }
-
-        //3. Alert dialog
-        clear= findViewById(R.id.clear)
-        clear.setOnClickListener {
-            val alert = AlertDialog.Builder(this)
-            alert.setTitle("Reset")
-            alert.setIcon(R.drawable.alert)
-            alert.setMessage("Are you sure you want to clear all entries?")
-            alert.setPositiveButton(R.string.yes) { dialog, which ->
-                pickDate.setText(null)
-                phone.setText(null)
-                showInfo.setText(null)
-                ccp.resetToDefaultCountry()
-            }
-            alert.setNegativeButton(R.string.no) { dialog, which ->
-                dialog.cancel()
-            }
-            alert.setNeutralButton(R.string.cancel) { dialog, which ->
-                dialog.cancel()
-            }
-            alert.show()
-        }
-
-
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        textView_massage.text = "Gender : "+Gender[position]
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
 
     }
 }
---
+++ app/src/main/java/com/sumaya/myapplication/MainFragment.kt
@@ -0,0 +1,136 @@
+package com.sumaya.myapplication
+
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import com.hbb20.CountryCodePicker
+import java.util.*
+
+
+
+class MainFragment : Fragment() {
+
+    //1. Date
+    private lateinit var pickDate: TextView
+
+    //2. Country Code
+    private lateinit var ccp: CountryCodePicker
+    private var countryCode: String? = null
+    private var countryName: String? = null
+
+
+    private lateinit var phone: EditText
+    private lateinit var name: EditText
+    private lateinit var gender: String
+    private lateinit var send: Button
+    private lateinit var clear: Button
+    private lateinit var date: String
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        // 1. Date Dialog
+        pickDate = view.findViewById(R.id.pickDate)
+
+        //create object of Calendar
+        val calendar = Calendar.getInstance()
+        // add day of month
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = calendar.get(Calendar.MONTH)
+        val year = calendar.get(Calendar.YEAR)
+        pickDate.setOnClickListener {
+            DatePickerDialog(view.context, DatePickerDialog.OnDateSetListener { _, y, m, d ->
+                date = "$d/$m/$y"
+                pickDate.setText(date)
+            }, year, month, day)
+                .show()
+        }
+
+        //2. Country Code
+        phone = view.findViewById(R.id.phone)
+        name = view.findViewById(R.id.name)
+        ccp = view.findViewById(R.id.pickCode)
+        ccp.setOnCountryChangeListener {
+            countryCode = ccp.selectedCountryCode
+            countryName = ccp.selectedCountryName
+        }
+
+        val spinner: Spinner = view.findViewById(R.id.gender)
+        ArrayAdapter.createFromResource(
+            view.context,
+            R.array.genderArray,
+            android.R.layout.simple_spinner_item
+        )
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = adapter
+            }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long
+            ) {
+                gender = spinner.selectedItem.toString()
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+            }
+        }
+
+
+        //3. Alert dialog
+        clear = view.findViewById(R.id.clear)
+        clear.setOnClickListener {
+            val alert = AlertDialog.Builder(view.context)
+            alert.setTitle("Reset")
+            alert.setIcon(R.drawable.alert)
+            alert.setMessage("Are you sure you want to clear all entries?")
+            alert.setPositiveButton(R.string.yes) { dialog, which ->
+                pickDate.setText(null)
+                phone.setText(null)
+                name.setText(null)
+                ccp.resetToDefaultCountry()
+            }
+            alert.setNegativeButton(R.string.no) { dialog, which ->
+                dialog.cancel()
+            }
+            alert.setNeutralButton(R.string.cancel) { dialog, which ->
+                dialog.cancel()
+            }
+            alert.show()
+        }
+
+        //show Info
+        send = view.findViewById(R.id.send)
+        send.setOnClickListener {
+            if (name.text.isNotEmpty() && pickDate.text.isNotEmpty() && phone.text.isNotEmpty()) {
+
+            var info = "Name: ${name.text}\n Gender: $gender\n Birthday: $date\n"
+            info += "Phone: +${countryCode.toString() + phone.text}\n"
+
+            parentFragmentManager.setFragmentResult("MyKey", bundleOf("data" to info))
+            parentFragmentManager.beginTransaction().replace(R.id.FragemtFrame, InfoFragment())
+                .addToBackStack("back")
+                .commit()
+        }else { Toast.makeText(context, "Complete all information", Toast.LENGTH_SHORT).show() }
+        }
+        return view
+    }
+}
+
+
